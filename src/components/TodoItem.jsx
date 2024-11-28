be1711a4d63e2a1426943e6cf9b900ed80ec350c
angus-chanan11 <angus.chan@oocl.com>
2024-11-28 22:03:41 +0800

    feat: add delete todo button

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,6 +1,17 @@
-const TodoItem = (props) => {
+import { useContext } from "react"
+import { TodoContext } from "../App"
+import { ACTION } from "../context/todoReducer"
+
+const TodoItem = (props) => {   
+    const {dispatch} = useContext(TodoContext)
+
+    const clickHandler = () => {
+        dispatch({type: ACTION.DELETE, payload: props.todo.id})
+    }
+
     return <div>
         <span>{props.todo.text}</span>
+        <button onClick={clickHandler}>X</button>
     </div>
 } 
 
--- src/context/todoReducer.js
+++ src/context/todoReducer.js
@@ -4,13 +4,16 @@ export const initialState = [
 ];
 
 export const ACTION = {
-  ADD: "ADD"
+  ADD: "ADD",
+  DELETE: "DELETE"
 };
 
 export const todoReducer = (state, action) => {
   switch (action.type){
     case ACTION.ADD:
       return [...state, {id: Date.now(), text: action.payload, done: false}]
+    case ACTION.DELETE:
+      return state.filter(todo => todo.id !== action.payload)
     default:
       return state
   }
