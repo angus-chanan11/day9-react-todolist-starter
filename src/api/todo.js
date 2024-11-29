863beda6bd818b55919db7d34d381c2fd77d86d6
angus-chanan11 <angus.chan@oocl.com>
2024-11-29 16:13:19 +0800

    refactor: rename path

--- src/api/todo.js
+++ src/api/todo.js
@@ -5,21 +5,23 @@ const instance = axios.create({
     timeout: 5000
 })
 
+const TODO_PATH_PREFIX = "/TodoItem";
+
 export const getTodoList = async () => {
-    const response = await instance.get("/TodoItem");
+    const response = await instance.get(TODO_PATH_PREFIX);
     return response.data;
 }
 
 export const addTodo = async (newTodo) => {
-    const response = await instance.post("/TodoItem", newTodo)
+    const response = await instance.post(TODO_PATH_PREFIX, newTodo)
     return response.data;
 }
 
 export const deleteTodo = async (todoId) => {
-    await instance.delete(`/TodoItem/${todoId}`)
+    await instance.delete(`${TODO_PATH_PREFIX}/${todoId}`)
 }
 
 export const updateTodo = async (id, {text, done}) => {
-    const response = await instance.put(`/TodoItem/${id}`, {id, text, done})
+    const response = await instance.put(`${TODO_PATH_PREFIX}/${id}`, {id, text, done})
     return response.data;
 }
