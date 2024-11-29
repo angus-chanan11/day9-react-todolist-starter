9b8cf364342f2411a6b4690612a68962337153b2
angus-chanan11 <angus.chan@oocl.com>
2024-11-29 16:21:17 +0800

    feat: add loading effect

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,25 +1,36 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import TodoGenerator from "./TodoGenerator";
 import TodoGroup from "./TodoGroup";
 import { getTodoList } from "../api/todo";
 import { TodoContext } from "../App";
 import { ACTION } from "../context/todoReducer";
+import { LoadingOutlined } from '@ant-design/icons';
+import { Spin } from 'antd';
 
 const TodoList = () => {
   const {dispatch} = useContext(TodoContext);
+  const [loading, setLoading] = useState(false);
   
   useEffect(() => {
+    setLoading(true);
+
     getTodoList()
       .then( (todos) => {
         dispatch({type: ACTION.SET, payload: todos})
+        setLoading(false);
       })
   }, [])
 
   return (
       <div>
-        <h1>Todo List</h1>
-        <TodoGroup/>
-        <TodoGenerator/>
+        {loading ? 
+          <Spin indicator={<LoadingOutlined spin />} /> : 
+          <>
+            <h1>Todo List</h1>
+            <TodoGroup/>
+            <TodoGenerator/>
+          </>
+        }
       </div>
   );
 }
